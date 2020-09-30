7aa6a35c09a2824ccc7c4428a5917f8de0aabed3
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-30 12:33:43 +0200

    ExA5 StartWith

---
+++ ExA5/src/test/java/be/abis/ExA1/ut/TestPerson.java
@@ -0,0 +1,30 @@
+package be.abis.ExA1.ut;
+import be.abis.ExA1.model.Person;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestPerson {
+
+    @Test
+    public void ageOfPersonShouldBe42 () {
+        Person p42 = new Person (1,"Griet","Costermans", LocalDate.of(1978, 6, 28));
+        int result = p42.calculateAge();
+        assertThat(result,equalTo(42));
+
+    }
+
+    @Test
+    public void toStringSentenceStartsWithPerson () {
+        //
+        Person pToString = new Person (1,"Griet","Costermans", LocalDate.of(1978, 6, 28));
+        String result = pToString.toString();
+        System.out.println(result);
+        assertThat(result, startsWith("Person"));
+
+    }
+}
