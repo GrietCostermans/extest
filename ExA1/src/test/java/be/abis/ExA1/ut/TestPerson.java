ba47156930b8e9be49343763ff0f3bd1306e1c07
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-09-30 11:13:13 +0200

    ExA22 = ExA2

---
+++ ExA1/src/test/java/be/abis/ExA1/ut/TestPerson.java
@@ -0,0 +1,17 @@
+package be.abis.ExA1.ut;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPerson {
+
+    @Test
+    public void ageOfPersonShouldBe42 () {
+        //Assert.fail();
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+}
